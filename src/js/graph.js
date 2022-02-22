7814837b3153e3bdbf7cbad69cf7eb4a1dfab47b
MaiconMares <maiconmaresunb@gmail.com>
2022-02-21 23:45:28 -0300

    feat: calculating distance based on lati/long, passing nodes with distance to MST and presenting results to user.

--- src/js/graph.js
+++ src/js/graph.js
@@ -1,86 +1,79 @@
-const Queue = require('./queue.js');
-const PriorityQueue = require('./priorityQueue.js');
-class Graph {
-    constructor(numNodes){
-        this.numNodes = numNodes;
-        this.adjacencyList = {}
+const Queue = require("./queue.js");
+const PriorityQueue = require("./priorityQueue.js");
+module.exports = class Graph {
+  constructor(numNodes) {
+    this.numNodes = numNodes;
+    this.adjacencyList = {};
+  }
+
+  addNode(v) {
+    if (!this.adjacencyList[v]) this.adjacencyList[v] = [];
+  }
+
+  addEdge(v, u, weight) {
+    this.adjacencyList[v].push({ node: u, weight: weight });
+    this.adjacencyList[u].push({ node: v, weight: weight });
+  }
+
+  primMST() {
+    const MST = new Graph();
+
+    let Nlist = Object.keys(this.adjacencyList);
+    let Elist = Object.values(this.adjacencyList);
+
+    if (Nlist.length === 0) {
+      return MST;
     }
 
-    addNode(v){
-        if(!this.adjacencyList[v]) this.adjacencyList[v] = [];
-    }
+    let s = Nlist[0];
+    let edgeQueue = new PriorityQueue();
+    let explored = new Set();
+    explored.add(s);
+    MST.addNode(s);
 
+    Elist[0].forEach((edge) => {
+      edgeQueue.PEnqueue([s, edge.node], edge.weight);
+    });
 
-    addEdge(v, u, weight){
-        this.adjacencyList[v].push({node: u, weight:weight});
-        this.adjacencyList[u].push({node: v, weight:weight});
+    let currentMinEdge = edgeQueue.PDequeue();
 
-    }
+    while (!edgeQueue.PQisEmpty()) {
+      while (
+        !edgeQueue.PQisEmpty() &&
+        explored.has(currentMinEdge.element[1])
+      ) {
+        currentMinEdge = edgeQueue.PDequeue();
+      }
 
-    primMST(){
-        const MST = new Graph();
+      let next = currentMinEdge.element[1];
+      let nextNode = Nlist.indexOf(next);
 
-
-          let Nlist = Object.keys(this.adjacencyList);
-          let Elist  = Object.values(this.adjacencyList);
-
-        if (Nlist.length === 0) {
-            return MST;
-        }
-
-        let s = Nlist[0];
-        let edgeQueue = new PriorityQueue();
-        let explored = new Set();
-        explored.add(s);
-        MST.addNode(s);
-
-
-
-        Elist[0].forEach(edge => {
-            edgeQueue.PEnqueue([s, edge.node], edge.weight);
+      if (!explored.has(next)) {
+        MST.addNode(next);
+        MST.addEdge(currentMinEdge.element[0], next, currentMinEdge.priority);
+        Elist[nextNode].forEach((edge) => {
+          edgeQueue.PEnqueue([next, edge.node], edge.weight);
         });
 
-        let currentMinEdge = edgeQueue.PDequeue();
-
-        while(!edgeQueue.PQisEmpty()){
-
-            while(!edgeQueue.PQisEmpty() && explored.has(currentMinEdge.element[1])){
-            currentMinEdge = edgeQueue.PDequeue();
-            }
-
-            let next = currentMinEdge.element[1];
-            let nextNode  = Nlist.indexOf(next);
-          console.log("nexttt:  " + next);
-          console.log("nextNode:  " + nextNode);
-
-            if (!explored.has(next)) {
-
-                MST.addNode(next);
-                MST.addEdge(currentMinEdge.element[0], next, currentMinEdge.priority);
-                Elist[nextNode].forEach(edge => {
-                   edgeQueue.PEnqueue([next, edge.node], edge.weight);
-            });
-
-                s = next;
-              explored.add(next);
-            }
-        }
-
-        return MST;
+        s = next;
+        explored.add(next);
+      }
     }
 
-    printGraph() {
-        var get_keys = Object.keys(this.adjacencyList)
+    return MST;
+  }
 
-        for (var i of get_keys){
-            var get_values = Object.values(this.adjacencyList[i]);
-            var conc = "";
+  printGraph() {
+    var get_keys = Object.keys(this.adjacencyList);
 
-            for (var j of get_values){
-                conc += JSON.stringify(j) + " ";
-            }
-            console.log(i + " -> " + conc);
-        }
+    for (var i of get_keys) {
+      var get_values = Object.values(this.adjacencyList[i]);
+      var conc = "";
 
+      for (var j of get_values) {
+        conc += JSON.stringify(j) + " ";
+      }
+      console.log(i + " -> " + conc);
     }
-}
+  }
+};
--- src/js/init.js
+++ src/js/init.js
@@ -1,93 +1,146 @@
-const prompt = require('prompt');
-const axios = require('axios');
-const XMLHttpRequest = require('xhr2');
+const prompt = require("prompt");
+const axios = require("axios");
+const XMLHttpRequest = require("xhr2");
+const Graph = require("./graph");
 
-const secretKey = '$2b$10$jxow4GADDMrolpxvMGbFcOqDcqkAYAykopUHA6Obtp9i8z0si6n3u';
+const secretKey =
+  "$2b$10$jxow4GADDMrolpxvMGbFcOqDcqkAYAykopUHA6Obtp9i8z0si6n3u";
 const url = `https://api.jsonbin.io/b/`;
 
 const endpoints = {
-    'df': `${url}62138f0aca70c44b6ea21400/latest`,
-}
-
-const options = {
-    method: 'GET',
-    url: 'https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/',
-    params: {ip: ''},
-    headers: {
-      'x-rapidapi-host': 'ip-geolocation-ipwhois-io.p.rapidapi.com',
-      'x-rapidapi-key': 'aafddca794mshce78d6092dfdf9fp1c67acjsn1a08597abf8c'
-    }
+  df: `${url}62138f0aca70c44b6ea21400/latest`,
 };
 
-const requestGeolocation = (publicIP) => {
-    options.params.ip = publicIP;
+const options = {
+  method: "GET",
+  url: "https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/",
+  params: { ip: "" },
+  headers: {
+    "x-rapidapi-host": "ip-geolocation-ipwhois-io.p.rapidapi.com",
+    "x-rapidapi-key": "aafddca794mshce78d6092dfdf9fp1c67acjsn1a08597abf8c",
+  },
+};
 
-    axios.request(options).then(response => {
-        console.log(response.data);
-    }).catch(error => {
-        console.error(error);
-    });
+function deg2rad(deg) {
+  return deg * (Math.PI / 180);
 }
 
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2 - lat1); // deg2rad below
+  var dLon = deg2rad(lon2 - lon1);
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) *
+      Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c; // Distance in km
+  return d;
+}
+
+const requestGeolocation = async (publicIP) => {
+  options.params.ip = publicIP;
+
+  let result = await axios
+    .request(options)
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error(error);
+    });
+
+  return { latitude: result.latitude, longitude: result.longitude };
+};
 
 const requestData = (uf) => {
+  const states = Object.keys(endpoints);
 
-    const states = Object.keys(endpoints);
+  if (!states.includes(uf)) {
+    console.log("Não temos dados para este estado!");
+    return;
+  }
 
-    if (!states.includes(uf)) {
-        console.log('Não temos dados para este estado!');
-        return;
+  return new Promise((resolve, reject) => {
+    const xml = new XMLHttpRequest();
+
+    xml.responseType = "json";
+    xml.onload = () => {
+      if (xml.status === 200) resolve(xml.response);
+      else reject("Erro interno!");
+    };
+    xml.open("GET", endpoints[uf]);
+    xml.setRequestHeader("secret-key", secretKey);
+    xml.send();
+  });
+};
+
+const passDataToGraph = async (data, yourLatitude, yourLongitude) => {
+  let g = new Graph();
+
+  let yourNode = {
+    LONGITUDE: yourLongitude,
+    LATITUDE: yourLatitude,
+    CNES: "9999999",
+  };
+
+  data.unshift(yourNode);
+
+  const vertices = [];
+  for (let i = 0; i < data.length; i++) {
+    if (
+      !isNaN(parseFloat(data[i]["LONGITUDE"])) &&
+      !isNaN(parseFloat(data[i]["LATITUDE"]))
+    ) {
+      vertices.push(data[i]["CNES"]);
+      g.addNode(vertices[i]);
     }
+  }
 
-    return new Promise((resolve, reject) => {
-        const xml = new XMLHttpRequest();
+  for (let j = 1; j < data.length; j++) {
+    let distance = getDistanceFromLatLonInKm(
+      parseFloat(data[0]["LATITUDE"]),
+      parseFloat(data[0]["LONGITUDE"]),
+      parseFloat(data[j]["LATITUDE"]),
+      parseFloat(data[j]["LONGITUDE"])
+    );
+    if (distance != 0) g.addEdge(vertices[0], vertices[j], distance);
+  }
+  g.primMST().printGraph();
 
-        xml.responseType = 'json';
-        xml.onload = () => {
-            if (xml.status === 200)
-                resolve(xml.response);
-            else
-                reject('Erro interno!');
-        }
-        xml.open('GET', endpoints[uf]);
-        xml.setRequestHeader('secret-key', secretKey);
-        xml.send();
-    });
-}
+  let ubsResultCodes = [];
+  let ubsResultData = [];
+  //Takes the ten first closest ubs to user location
+  for (let z = 0; z < 10; z++)
+    ubsResultCodes.push(g.primMST().adjacencyList["9999999"][z]["node"]);
 
-const transformData = async (data) => {
-    //Fazer transformações necessárias
+  for (let k = 1; k < data.length; k++) {
+    if (ubsResultCodes.includes(data[k]["CNES"]))
+        ubsResultData.push(data[k]);
+  }
 
-    return data;
-}
+  let radius = g.primMST().adjacencyList["9999999"][0]["weight"];
+
+  console.log(`UBS MAIS PRÓXIMAS EM UM RAIO DE ${radius}`);
+
+  console.log(ubsResultData);
+};
 
 const init = async () => {
-    prompt.start();
+  prompt.start();
 
-    prompt.get(['state', 'ip'], function (err, result) {
+  console.log("SEJA BEM-VINDO(A)!")
+  console.log("INFORME O SEU ESTADO (APENAS SIGLA) E IP PÚBLICO (NÚMEROS E PONTUAÇÃO):");
+  prompt.get(["state", "ip"], async function (err, result) {
+    let location = await requestGeolocation(result.ip);
 
-        console.log('Command-line input received:');
-        console.log('  state: ' + result.state);
-        console.log('  ip: ' + result.ip);
-
-        requestGeolocation(result.ip);
-
-        requestData(result.state).then(async onfulfilled => {
-            let deserializedData = await transformData(onfulfilled);
-
-
-            /*
-            //Mandar dados para a estrutura de dados e gerar a MST
-            graphStructure(await deserializedData);
-
-            //Percorrer MST e retornar UBS's para o usuário
-            queryData();
-            */
-        }).catch(onrejected => console.log(onrejected));
-        return;
-    });
-
-
-}
+    requestData(result.state)
+      .then(async (ubs_data) => {
+        passDataToGraph(ubs_data, location.latitude, location.longitude);
+      })
+      .catch((onrejected) => console.log(onrejected));
+    return;
+  });
+};
 
 init();
